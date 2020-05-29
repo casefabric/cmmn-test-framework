1c870beb79e35860ce4c1c681bcf6ca085eea6db
Thijs Petter <thijspetter@gmail.com>
2020-05-29 19:02:52 +0200

    Adjusted for case team route refactoring

--- src/framework/cmmn/caseteammember.ts
+++ src/framework/cmmn/caseteammember.ts
@@ -1,14 +1,15 @@
 import User from "../user";
 
 export default class CaseTeamMember {
-    user: string;
+    memberId: string;
 
     /**
      * 
      * @param user Either a String or a User, holding the user id of the case team member
+     * @param memberType The type of member (either a 'user' or a 'role')
      * @param roles Set of roles that the user has within this case team; if not given, then the roles of the user are used (if any)
      */
-    constructor(user: any, public roles: string[] = []) {
-        this.user = user instanceof User ? user.id : user;
+    constructor(user: any, public memberType: string = 'user', public isOwner: boolean = false, public caseRoles: string[] = []) {
+        this.memberId = user instanceof User ? user.id : user;
     }
 }
