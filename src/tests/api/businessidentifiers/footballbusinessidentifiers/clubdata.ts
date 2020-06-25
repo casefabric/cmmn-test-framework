a68bb25ecf124971017b522456bd8f9c40109d17
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-06-25 18:04:36 +0530

    Added test football business identifiers with combined models
    Now, TestFootballBusinessIdentifiers does
    - business identifiers filters test on single model
    - business identifiers filters test on single model (multi-user)
    - combined business identifiers filters test on 2 models
    - combined business identifiers filters test on 2 models (muli-user)

---
+++ src/tests/api/businessidentifiers/footballbusinessidentifiers/clubdata.ts
@@ -0,0 +1,124 @@
+export default class ClubData {
+    static clubData = [
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Lewandowski'   
+        },
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Kimmich'   
+        },
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Robben'   
+        },
+        {
+            clubname: 'Real Madrid',
+            age: 118,
+            country: 'Spain',
+            name: 'Robben'   
+        },
+        {
+            clubname: 'Barcelona',
+            age: 121,
+            country: 'Spain',
+            name: 'Left'   
+        },
+        {
+            clubname: 'Real Madrid',
+            age: 118,
+            country: 'Spain',
+            name: 'Ronaldo'   
+        },
+        {
+            clubname: 'Juventus',
+            age: 123,
+            country: 'Italy',
+            name: 'Ronaldo'   
+        },
+        {
+            clubname: 'Manchester United',
+            age: 142,
+            country: 'England',
+            name: 'Ronaldo'   
+        },
+        {
+            clubname: 'Manchester United',
+            age: 142,
+            country: 'England',
+            name: 'Robin van Persie'   
+        },
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Franck Ribery'   
+        },
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Manuel Neuer'   
+        },
+        {
+            clubname: 'Manchester United',
+            age: 142,
+            country: 'England',
+            name: 'Rashford'   
+        },
+        {
+            clubname: 'Barcelona',
+            age: 121,
+            country: 'Spain',
+            name: 'Iniesta'   
+        },
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Goretzka'   
+        },
+        {
+            clubname: 'Juventus',
+            age: 123,
+            country: 'Italy',
+            name: 'Pirlo'   
+        },
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Lahm'   
+        },
+        {
+            clubname: 'Real Madrid',
+            age: 118,
+            country: 'Spain',
+            name: 'Modric'   
+        },
+        {
+            clubname: 'Bayern Munich',
+            age: 120,
+            country: 'Germany',
+            name: 'Davies'   
+        },
+        {
+            clubname: 'Juventus',
+            age: 123,
+            country: 'Italy',
+            name: 'Dybala'   
+        },
+        {
+            clubname: false,
+            age: 999,
+            country: true,
+            name: false   
+        },
+    ]
+}
