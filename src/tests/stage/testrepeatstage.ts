5e91c6a8d258b9b25202c58581c9ce02ebeba83e
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-08-25 08:23:16 +0530

    Added TestRepeatStage

---
+++ src/tests/stage/testrepeatstage.ts
@@ -0,0 +1,102 @@
+'use strict';
+
+import RepositoryService from "../../framework/service/case/repositoryservice";
+import CaseService from "../../framework/service/case/caseservice";
+import WorldWideTestTenant from "../worldwidetesttenant";
+import MockServer from "../../framework/mock/mockserver";
+import GetMock from "../../framework/mock/getmock";
+import TestCase from "../../framework/test/testcase";
+import Case from "../../framework/cmmn/case";
+import { assertPlanItemState, assertCaseFileContent } from "../../framework/test/assertions";
+import CasePlanService from "../../framework/service/case/caseplanservice";
+import { ServerSideProcessing, SomeTime } from "../../framework/test/time";
+import CaseFileService from "../../framework/service/case/casefileservice";
+import { TenantOwner } from "../../framework/tenant/tenantuser";
+
+const repositoryService = new RepositoryService();
+const caseService = new CaseService();
+const casePlanService = new CasePlanService();
+const worldwideTenant = new WorldWideTestTenant();
+
+const definition = 'repeatstagetest.xml';
+const tenant = worldwideTenant.name;
+const user = worldwideTenant.sender;
+
+const mockPort = 27384;
+const mock = new MockServer(mockPort);
+
+let count = 0;
+
+function getTimerData(index: any) {
+    // const response = {
+    //     TimeRetry: `P0DT0H0M${30 * count}S`
+    // }
+    
+    const localCount = Number.parseInt(index);
+    console.log(" - Count is now " + localCount)
+    const response = {
+        TimeRetry: `P0DT0H0M${1 + 3 * localCount}S`
+    }
+    console.log(" - Returning timer response with data " + response.TimeRetry)
+    count ++;
+    return response;
+}
+
+const pingMock = new GetMock(mock, '/getWaitTime', call => {
+    // Return immediately a code 200
+    const index = call.req.query.index;
+    console.log(" - Getting ping["+count+"] on index " + index);
+    call.json(getTimerData(index));
+    call.setSyncMessage("Received ping " + index);
+});
+
+export default class TestRepeatStage extends TestCase {
+    // private 
+    async onPrepareTest() {
+        console.log("Starting mock servive in test preparation");
+        await mock.start();
+        console.log("\n\n============Started mock server. Now creating tenant\n\n");
+        await worldwideTenant.create();
+        // Deploy the case model
+        await repositoryService.validateAndDeploy(definition, user, tenant);
+    }
+
+    async run() {
+        const inputs = {
+            HTTPPort: {
+                port: mockPort
+            }
+        }
+
+        const startCase = { tenant, definition, inputs };
+        const caseInstance = await caseService.startCase(startCase, user) as Case;
+
+
+        // Let the Status stage repeat for 3 times
+        for(let i = 0; i < 3; i++) {
+            console.log("\n=== Awaiting mock["+i+"]")
+
+            const sync = await pingMock.untilCallInvoked(50000);
+            console.log("=== " + sync)
+
+            await ServerSideProcessing();
+            await SomeTime(1000);
+
+            await caseService.getCase(caseInstance, user).then(caze => {
+                console.log("Case now has " + caze.planitems.length +"  plan items");
+                caze.planitems.filter(p => p.name === 'Status').forEach(s => {
+                    console.log(`Status[${s.index}] is in state ${s.currentState}\thaving id: ${s.id}`)
+                    // console.log("S: " + JSON.stringify(s, undefined, 2))
+                })
+            })
+
+            // Status stage of index i must be completed
+            await assertPlanItemState(caseInstance, 'Status', i, user, 'Completed');
+        }
+
+        // Make transistion on Kill user event
+        await casePlanService.makePlanItemTransition(caseInstance, user, 'Kill', 'Occur');
+
+        console.log("Case ID:\t" + caseInstance.id);
+    }
+}
