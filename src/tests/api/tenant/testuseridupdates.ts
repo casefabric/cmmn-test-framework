20e84f49d52479a23f43df1569a225bfd7d777b6
Thijs Petter <thijspetter@gmail.com>
2021-01-22 18:11:24 +0100

    Update test case to validate lastModified is not changed

--- src/tests/api/tenant/testuseridupdates.ts
+++ src/tests/api/tenant/testuseridupdates.ts
@@ -15,6 +15,7 @@ import StartCase from "../../../framework/service/case/startcase";
 import CaseTeamService from "../../../framework/service/case/caseteamservice";
 import CaseHistoryService from "../../../framework/service/case/casehistoryservice";
 import { SomeTime } from "../../../framework/test/time";
+import logger from "../../../framework/logger";
 
 const platformAdmin = new User('admin');
 
@@ -32,8 +33,8 @@ const tenantUser2 = new TenantUser('user-2-' + guid, tenantRoles);
 const tenantUser3 = new TenantUser('user-3-' + guid, tenantRoles);
 
 // New IDs
-const newUser1Id = 'user1_' + guid;
-const newUser3Id = 'user3_' + guid;
+const newTenantUser1 = new TenantUser('user1_' + guid, tenantRoles);
+const newTenantUser3 = new TenantUser('user3_' + guid, tenantRoles);
 
 const tenant1 = new Tenant(tenant1Name, [tenantOwner, tenantUser1, tenantUser2, tenantUser3]);
 const tenant2 = new Tenant(tenant2Name, [tenantOwner, tenantUser1, tenantUser2]);
@@ -45,26 +46,32 @@ const caseService = new CaseService();
 const taskService = new TaskService();
 const caseHistoryService = new CaseHistoryService();
 
-
 export default class TestUserIdUpdates extends TestCase {
     /**
      * @override
      */
     async run() {
+        // Create a basic infra of 2 tenants and 5 cases.
         await this.createTenants();
         await this.createCases();
+
+        // It should not be possible to change into existing users.
         await this.invalidChangeUser();
-        await this.changeUser([tenantUser1, tenantUser3], [new TenantUser(newUser1Id), new TenantUser(newUser3Id)]);
-        await this.changeUser([new TenantUser(newUser1Id), new TenantUser(newUser3Id)], [tenantUser1, tenantUser3]);
+        // First, change 'user-1-guid' into 'user1_guid'
+        await this.changeUsers([tenantUser1], [newTenantUser1]);
+        // After that, change user 3
+        await this.changeUsers([tenantUser3], [newTenantUser3]);
+        // Finally, revert back to the original user id's in one shot.
+        await this.changeUsers([newTenantUser1, newTenantUser3], [tenantUser1, tenantUser3]);
     }
 
-    async changeUser(users: Array<TenantUser>, newUsers: Array<TenantUser>) {
+    async changeUsers(users: Array<TenantUser>, newUsers: Array<TenantUser>) {
         const newInfo = [];
         const beforeStats = [];
         const afterStats = [];
 
         // Getting all the users stats before update
-        for(let i=0; i<users.length; i++) {
+        for (let i = 0; i < users.length; i++) {
             const info = {
                 existingUserId: users[i].userId,
                 newUserId: newUsers[i].userId
@@ -78,25 +85,45 @@ export default class TestUserIdUpdates extends TestCase {
 
         // Updating user-ids via platform admin
         await platformService.updateUserInformation(platformAdmin, newInfo);
-        
+
         await SomeTime(5000);
 
         // Getting all the users stats after update
-        for(let newUser of newUsers) {
+        for (let i = 0; i < newUsers.length; i++) {
+            const newUser = newUsers[i];
+            // for (let newUser of newUsers) {
             await newUser.login();
-            // const newUserInfo = await platformService.getUserInformation(newUser);
-            
+            // // It should not be possible to login with original user anymore
+            // await users[i].login();
+
             // Get user stats after updating user-id
             const afterUserStats = await this.getUserStats(newUser);
+
+            // Method that converts a user id to the old one, if it was changed.
+            //  This is needed to make the comparison of modifiedBy to work properly.
+            const convertId = (potentialNewUserId: string) => {
+                for (let k = 0; k < newUsers.length; k++) {
+                    if (potentialNewUserId === newUsers[k].userId) {
+                        return users[k].userId;
+                    }
+                }
+                return potentialNewUserId
+            };
+            // Run through the list of user id's and change the id of the newly changed users back to the old one
+            //  in order to keep the comparison the same.
+            const afterStatsLastModified = afterUserStats.lastModifiedSet;
+            for (let j = 0; j<afterStatsLastModified.length; j++) {
+                afterStatsLastModified[j] = convertId(afterStatsLastModified[j]);
+            }
             afterStats.push(afterUserStats);
         }
 
         // Comparing before and after update stats
-        for(let i=0; i<users.length; i++) {
-            const beforeMsg = JSON.stringify({user: users[i].userId, beforeStats: beforeStats[i]}, undefined, 2);
-            const afterMsg = JSON.stringify({user: newUsers[i].userId, afterStats: afterStats[i]}, undefined, 2);
+        for (let i = 0; i < users.length; i++) {
+            const beforeMsg = JSON.stringify({ user: users[i].userId, beforeStats: beforeStats[i] }, undefined, 2);
+            const afterMsg = JSON.stringify({ user: newUsers[i].userId, afterStats: afterStats[i] }, undefined, 2);
 
-            if(!Comparison.sameJSON(beforeStats, afterStats)) {
+            if (!Comparison.sameJSON(beforeStats, afterStats)) {
                 throw new Error(`Mismatching statistics for user ${users[i].userId};\nExpected:${beforeMsg},\nFound:${afterMsg}`);
             }
         }
@@ -124,9 +151,11 @@ export default class TestUserIdUpdates extends TestCase {
 
         // get the number of cases that user is part of
         const caseIds: Array<string> = [];
+        const lastModifiedSet: Array<string> = [];
         const cases = await caseService.getCases(user).then(res => {
             res.forEach(c => {
                 caseIds.push(c.id);
+                lastModifiedSet.push(c.modifiedBy);
                 console.log(`Found case [${c.id}] for user ${user.id} in tenant ${c.tenant}`)
             });
             return res.length;
@@ -135,9 +164,9 @@ export default class TestUserIdUpdates extends TestCase {
 
         for (const id of caseIds) {
             await caseTeamService.getCaseTeam(user, id).then(team => {
-                console.log("Members in case " + id +": " + team.members.map(m => m.memberId + " (of type: " + m.memberType +")"))
+                console.log("Members in case " + id + ": " + team.members.map(m => m.memberId + " (of type: " + m.memberType + ")"))
             })
-            const planItemHistory = await caseHistoryService.getCasePlanHistory(user, {id} as Case).then(items => items.length);
+            const planItemHistory = await caseHistoryService.getCasePlanHistory(user, { id } as Case).then(items => items.length);
             planItemHistories += planItemHistory;
         }
         // await caseService.getCases(user).then(res => console.log(JSON.stringify(res, undefined, 2)));
@@ -145,7 +174,7 @@ export default class TestUserIdUpdates extends TestCase {
         // get the number of tasks that user is part of
         const tasks = await taskService.getTasks(user).then(res => res.length);
 
-        return {tenants, cases, tasks, planItemHistories};
+        return { tenants, cases, tasks, planItemHistories, lastModifiedSet };
     };
 
     async createCases() {
@@ -156,30 +185,30 @@ export default class TestUserIdUpdates extends TestCase {
             }
         };
         const caseTeam = new CaseTeam([new CaseOwner(tenantUser1), new CaseTeamMember('role1', [], 'role')]);
-        
-        const caseInstanceId = 'case1-'+guid;
+
+        const caseInstanceId = 'case1-' + guid;
         const startCase = { tenant: tenant1.name, definition, inputs, caseTeam, caseInstanceId };
 
         // Create a case and complete the first task
         await this.createCaseAndCompleteTask(tenantUser1, tenantUser2, startCase);
-        startCase.caseInstanceId = 'case2-'+guid;
+        startCase.caseInstanceId = 'case2-' + guid;
         await this.createCaseAndCompleteTask(tenantUser1, tenantUser3, startCase);
 
         // Also create a case in second tenant.
         startCase.tenant = tenant2.name;
-        startCase.caseInstanceId = 'case3-'+guid;
+        startCase.caseInstanceId = 'case3-' + guid;
         await this.createCaseAndCompleteTask(tenantUser1, tenantUser2, startCase);
 
         // Starting a case with actual user-ids tenantUser1, tenantUser3
         const caseTeam2 = new CaseTeam([new CaseOwner(tenantUser1), new CaseTeamMember(tenantUser3)]);
         const startCase2 = { tenant: tenant1.name, definition, inputs, caseTeam: caseTeam2, caseInstanceId };
-        startCase2.caseInstanceId = 'case4-'+guid;
+        startCase2.caseInstanceId = 'case4-' + guid;
         await caseService.startCase(tenantUser1, startCase2) as Case;
 
         // Starting a case with actual user-ids tenantUser1, tenantUser2, tenantUser3
         const caseTeam3 = new CaseTeam([new CaseOwner(tenantUser1), new CaseTeamMember(tenantUser3), new CaseTeamMember(tenantUser2)]);
         // console.log(JSON.stringify(caseTeam2, undefined, 2))
-        startCase2.caseInstanceId = 'case5-'+guid;
+        startCase2.caseInstanceId = 'case5-' + guid;
         startCase2.caseTeam = caseTeam3;
         await caseService.startCase(tenantUser1, startCase2) as Case;
 
@@ -199,7 +228,7 @@ export default class TestUserIdUpdates extends TestCase {
         const caze = await caseService.startCase(creator, startCase) as Case;
         const tasks = await taskService.getCaseTasks(completer, caze);
         const task = tasks.find(task => Task.isActive(task));
-        if (! task) throw new Error('Expected to find an active task');
+        if (!task) throw new Error('Expected to find an active task');
         await taskService.claimTask(completer, task);
         await taskService.completeTask(completer, task, taskOutput);
     }
