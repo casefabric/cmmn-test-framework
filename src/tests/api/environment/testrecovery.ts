7109ac5a51bbe16b9becc79754a0e304976e5676
Thijs Petter <thijspetter@gmail.com>
2023-05-03 18:13:18 +0200

    Extend recovery test with migrated cases

--- src/tests/api/environment/testrecovery.ts
+++ src/tests/api/environment/testrecovery.ts
@@ -3,11 +3,14 @@ import DebugService from "@cafienne/typescript-client/service/case/debugservice"
 import RepositoryService from "@cafienne/typescript-client/service/case/repositoryservice";
 import TaskService from "@cafienne/typescript-client/service/task/taskservice";
 import TenantService from "@cafienne/typescript-client/service/tenant/tenantservice";
-import Tenant from "@cafienne/typescript-client/tenant/tenant";
-import TenantUser, { TenantOwner } from "@cafienne/typescript-client/tenant/tenantuser";
-import { findTask } from "@cafienne/typescript-client/test/caseassertions/task";
 import TestCase from "@cafienne/typescript-client/test/testcase";
 import WorldWideTestTenant from "../../worldwidetesttenant";
+import CaseTeam from "@cafienne/typescript-client/cmmn/team/caseteam";
+import { CaseOwner, DefinitionMigration, findTask } from "@cafienne/typescript-client";
+import CaseTeamUser from "@cafienne/typescript-client/cmmn/team/caseteamuser";
+import CaseTeamService from "@cafienne/typescript-client/service/case/caseteamservice";
+import CasePlanService from "@cafienne/typescript-client/service/case/caseplanservice";
+import CaseMigrationService from "@cafienne/typescript-client/service/case/casemigrationservice";
 
 const worldwideTenant = new WorldWideTestTenant();
 const definition = 'helloworld.xml';
@@ -25,9 +28,11 @@ export default class TestRecovery extends TestCase {
      * @override
      */
     async run() {
-        await this.runTenantRecovery();
+        // await this.runTenantRecovery();
 
-        await this.runCaseRecovery();
+        // await this.runCaseRecovery();
+
+        await this.runMigratedCaseRecovery();
     }
 
     async runTenantRecovery() {
@@ -48,7 +53,7 @@ export default class TestRecovery extends TestCase {
         // Here you can fill your own case id. If not given, then a random id will be retrieved.
         const caseId = await this.getRandomCaseId();
         // const caseId = '';
-        if (! caseId) {
+        if (!caseId) {
             console.log(`User ${user.id} has no cases to recover`);
             return;
         }
@@ -66,10 +71,37 @@ export default class TestRecovery extends TestCase {
             return cases[0].id;
         } else {
             // Create a simple helloworld case and complete the first task in it
-            const newCase = await CaseService.startCase(user,{ tenant, definition, inputs : { Greeting: { Message: 'Hello there', From: user.id }}});
-            const task = await TaskService.getCaseTask(user, newCase, 'Receive Greeting and Send response');    
-            await TaskService.completeTask(user, task, { Response: { Message: 'Toedeledoki' }});
+            const newCase = await CaseService.startCase(user, { tenant, definition, inputs: { Greeting: { Message: 'Hello there', From: user.id } } });
+            const task = await TaskService.getCaseTask(user, newCase, 'Receive Greeting and Send response');
+            await TaskService.completeTask(user, task, { Response: { Message: 'Toedeledoki' } });
             return newCase.id;
         }
+    }
+
+    async runMigratedCaseRecovery() {
+        const startCase = {
+            tenant,
+            definition: 'helloworld_base.xml',
+            inputs: { Greeting: { Message: 'Hello there', From: user.id } },
+            caseTeam: new CaseTeam([new CaseOwner(user, ["ADMIN", "TempRole"]), new CaseTeamUser(worldwideTenant.receiver, ["OneMore"])])
+        };
+
+        const case1_before = await CaseService.startCase(user, startCase).then(instance => CaseService.getCase(user, instance));
+
+        const firstTaskName = 'Receive Greeting and Send response';
+        const taskOutput = { Response: { Message: 'Toedeledoki', } };
+        const migratedDefinition = new DefinitionMigration("helloworld_migrated.xml");
+        const case1Tasks_before = await TaskService.getCaseTasks(user, case1_before);
+        const case1FirstTask = findTask(case1Tasks_before, firstTaskName);
+
+        // Migrate caseInstance1, and then complete the task in case1
+        const case1_after = await CaseMigrationService.migrateDefinition(user, case1_before, migratedDefinition).then(() => CaseService.getCase(user, case1_before));
+        await TaskService.completeTask(user, case1FirstTask, taskOutput);
+
+        // Remove case from memory
+        await DebugService.forceRecovery(user, case1_before.id);
+
+        // Reload it by getting the discretionary items
+        await CaseService.getDiscretionaryItems(user, case1_before);
     }
 }
